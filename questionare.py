98de1378c18b7a54cd57a5732614f6c96456c49e
aSparaLaraguss <orbh8609@outlook.com>
2026-01-15 12:31:48 +0000

    Initialised

---
+++ questionare.py
@@ -0,0 +1,20 @@
+"""
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello/<int:planet>')
+def hello(planet):
+    return render_template('survey.html', t_planet=planet)
+
+@app.route('/form', methods=['GET'])
+def form():
+    if request.args.get('submit'):
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        return f'First Name: {first_name}, Last Name: {last_name}'
+    return render_template('form.html')
+"""
